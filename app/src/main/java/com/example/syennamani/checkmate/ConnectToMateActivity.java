d1b38db595ccdf0b652ed3d23d5bcf0870eaad8b
syennamani <syennamani@tracfone.com>
2017-04-14 18:47:02 -0400

    Friends List UI

--- app/src/main/java/com/example/syennamani/checkmate/ConnectToMateActivity.java
+++ app/src/main/java/com/example/syennamani/checkmate/ConnectToMateActivity.java
@@ -1,19 +1,50 @@
 package com.example.syennamani.checkmate;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ConnectToMateActivity extends BaseActivity {
 
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connecttomate);
+        context = this;
+        // FriendsList List View
+        ListView listView = (ListView)findViewById(R.id.lv_friendsList);
+        String[] values = new String[] {"Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile"};
 
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+
+        FriendsListAdapter adapter = new FriendsListAdapter(context,list);
+        listView.setAdapter(adapter);
+
+        // Add Friend Floating action button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +56,47 @@ public class ConnectToMateActivity extends BaseActivity {
                 showCustomDialog("");
             }
         });
+
+    }
+
+    private class FriendsListAdapter extends BaseAdapter {
+
+        private Context context;
+        private List<String> friends;
+        public FriendsListAdapter(Context context,
+                                  List<String> friends) {
+            this.context = context;
+            this.friends = friends;
+        }
+
+        @Override
+        public int getCount() {
+            return friends.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return friends.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            // First let's verify the convertView is not null
+            if (convertView == null) {
+                // This a new view we inflate the new layout
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.adapter_friendslist, parent, false);
+            }
+            TextView tvTextBottom = (TextView)convertView.findViewById(R.id.card_item_textBottom);
+            tvTextBottom.setText(friends.get(position));
+            return convertView;
+        }
 
     }
 
